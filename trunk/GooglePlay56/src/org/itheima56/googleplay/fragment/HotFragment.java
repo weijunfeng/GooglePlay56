24e04559451f85205b572f0d6c38d4a3bbddd5de
xq <xq@fdaa96b9-78cf-064e-8c58-764717d1f67d>
2015-05-07 06:40:13 +0000

    [code]排行页面随机色的生成
    git-svn-id: https://desktop-feedbts/svn/GooglePlay56@34 fdaa96b9-78cf-064e-8c58-764717d1f67d

--- trunk/GooglePlay56/src/org/itheima56/googleplay/fragment/HotFragment.java
+++ trunk/GooglePlay56/src/org/itheima56/googleplay/fragment/HotFragment.java
@@ -1,12 +1,17 @@
 package org.itheima56.googleplay.fragment;
 
 import java.util.List;
+import java.util.Random;
 
+import org.itheima56.googleplay.R;
 import org.itheima56.googleplay.fragment.LoadingPager.LoadedResult;
 import org.itheima56.googleplay.http.HotProtocol;
 import org.itheima56.googleplay.utils.UIUtils;
 import org.itheima56.googleplay.widget.FlowLayout;
 
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -37,6 +42,9 @@ public class HotFragment extends BaseFragment
 
 		// 加载流式布局
 		FlowLayout layout = new FlowLayout(UIUtils.getContext());
+		layout.setSpace(UIUtils.dip2px(4), UIUtils.dip2px(4));
+		layout.setPadding(UIUtils.dip2px(4), UIUtils.dip2px(4), UIUtils.dip2px(4), UIUtils.dip2px(4));
+
 		rootView.addView(layout);
 
 		// 给流式加载数据
@@ -46,6 +54,27 @@ public class HotFragment extends BaseFragment
 
 			TextView tv = new TextView(UIUtils.getContext());
 			tv.setText(data);
+			tv.setTextSize(UIUtils.dip2px(16));
+			tv.setTextColor(Color.WHITE);
+			tv.setGravity(Gravity.CENTER);
+			int padding = UIUtils.dip2px(4);
+			tv.setPadding(padding, padding, padding, padding);
+
+			Random rdm = new Random();
+			int shape = GradientDrawable.RECTANGLE;
+			int radius = UIUtils.dip2px(4);
+			int alpha = 0xff;// 0-255
+			int red = rdm.nextInt(170) + 30;// 0-255 30--->200
+			int green = rdm.nextInt(170) + 30;// 0-255
+			int blue = rdm.nextInt(170) + 30;// 0-255
+			int argb = Color.argb(alpha, red, green, blue);
+
+			GradientDrawable gd = new GradientDrawable();
+			gd.setShape(shape);// 设置形状
+			gd.setCornerRadius(radius);// 设置圆角
+			gd.setColor(argb);
+
+			tv.setBackgroundDrawable(gd);
 
 			layout.addView(tv);
 		}
